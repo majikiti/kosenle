5163ab140ef301ea03409c5f499f3e26e229c92f
YamaD <me@dyama.net>
2022-02-23 11:23:47 +0900

    initialized

---
+++ src/app/app.tsx
@@ -0,0 +1,12 @@
+import "./app.scss"
+
+export function App() {
+  return (
+    <>
+      <h1>Hello Vite + Preact!</h1>
+      <p>
+        <a class='link' href="https://preactjs.com/">Learn Preact</a>
+      </p>
+    </>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import { render } from "preact"
+import { App } from "./app/app"
+
+render(<App />, document.getElementById("app")!)

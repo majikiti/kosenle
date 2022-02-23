ca51ffbade63b0806a286a97b7ae43f8db39de91
YamaD <me@dyama.net>
2022-02-23 21:43:22 +0900

    updated app layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,65 @@
+import { FunctionComponent } from "preact"
+import { useState } from "preact/hooks"
 import Layout from "@lib/layout"
+import "./app.scss"
+
+type Box = {
+  char?: string
+  state: BoxState
+}
+
+type BoxState = "secret" | "correct" | "present" | "absent"
+
+const BoxElem: FunctionComponent<Box> = ({ char, state }) => (
+  <div class={`box ${state}`}>
+    {state != "secret" && <span class='char'>{char}</span>}
+  </div>
+)
+
+const Game: FunctionComponent = () => {
+  // width: 4 or 5
+  // height: 5
+  const maxWidth = 5
+  const height = 6
+
+  const tab: Box[][] = [...Array(height)].map(() =>
+    [...Array(maxWidth)].map(() => ({ state: "secret" }))
+  )
+
+  const [input, setInput] = useState("")
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    setInput("")
+  }
+
+  return (
+    <>
+      <div class='tab'>
+        {tab.map(row => (
+          <div class='row'>
+            {row.map(box => (
+              <BoxElem {...box} />
+            ))}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          class='input'
+          placeholder='hoge高専'
+          value={input}
+          onChange={({ value }) => setInput(value)}
+        />
+      </form>
+    </>
+  )
+}
 
 export function App() {
   return (
     <Layout>
-      Game
+      <Game />
     </Layout>
   )
 }

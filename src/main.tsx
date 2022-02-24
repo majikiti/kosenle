f43eb83a9c6c936d572ae04f0850508df41b233e
YamaD <me@dyama.net>
2022-02-24 21:20:13 +0900

    added greeting

--- src/main.tsx
+++ src/main.tsx
@@ -2,3 +2,8 @@ import { render } from "preact"
 import { App } from "./app/app"
 
 render(<App />, document.getElementById("app")!)
+
+console.log(
+  "%cこんにちは!",
+  "color: #FF0000; font-size: 8em; font-weight: bold;"
+)
